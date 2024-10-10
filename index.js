a651593f6afd6fc69385d3b2bed0e5dcbd3ea4d9
blessed wesonga <blessedwesonga@gmail.com>
2024-10-10 19:16:57 +0300

    phase-0-the-dom-modifying-elements-la

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+const main = document.getElementById('main');
+if (main) {
+    main.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Your-Name is the champion'; 
+
+document.body.appendChild(newHeader);
+
